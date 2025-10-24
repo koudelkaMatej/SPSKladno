7a9e1c7dc3da6f5de13afc0c780662395a008da2
Ing. Matěj Koudelka <koudelka@ad.spskladno.cz>
2025-10-24 10:41:51 +0200

    agar

---
+++ PyGame/Agar_io/Blob.py
@@ -0,0 +1,15 @@
+import pygame
+from settings import *
+import random
+
+class Blob(pygame.sprite.Sprite):
+    def __init__(self):    
+        super().__init__()  # Initialize the pygame.sprite.Sprite base class
+        self.x = random.randint(BLOB_START_SIZE, SCREEN_WIDTH- BLOB_START_SIZE)
+        self.y = random.randint(BLOB_START_SIZE, SCREEN_HEIGHT - BLOB_START_SIZE)
+        self.radius = BLOB_START_SIZE
+        self.color = LIGHT_GREEN
+        self.image = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.image.fill((0, 0, 0, 0))  # Clear the surface
+        pygame.draw.circle(self.image, self.color, (self.radius, self.radius), self.radius)
--- PyGame/Fire And Water/main.py
+++ PyGame/Fire And Water/main.py
@@ -1,54 +1,25 @@
 import pygame
 from pygame.locals import *
 from settings import *
-from World import World
 import random
-from Player import Player
 import os
 pygame.init()
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Oheň a voda')
-
-def draw_grid():
-	for line in range(0, SCREEN_WIDTH // TILE_SIZE + 1):
-		pygame.draw.line(screen, (255, 255, 255), (0, line * TILE_SIZE), (SCREEN_WIDTH, line * TILE_SIZE))
-		pygame.draw.line(screen, (255, 255, 255), (line * TILE_SIZE, 0), (line * TILE_SIZE, SCREEN_HEIGHT))
-
-# map from csv
-with open('mapa.csv', 'r') as f:
-	world_data = f.readlines()
-	world_data = [list(map(int, row.strip().split(';'))) for row in world_data]
-
-world = World(world_data)
-
-sun_img = pygame.image.load(SUN_IMAGE_PATH)
-random_sun_x = random.randint(50, SCREEN_WIDTH - sun_img.get_width() - (SCREEN_WIDTH - world_data[0].count(1) * TILE_SIZE))
-bg_img = pygame.image.load(SKY_IMAGE_PATH)
-
-def draw_bg():
-	screen.blit(bg_img, (0, 0))
-	screen.blit(sun_img, (random_sun_x, 100))
-
-folder_path = PLAYER_IMAGE_IDLE_PATH
-files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-num_files = len(files)  # Count the number of files
-print(num_files)
-
-player = Player()
-group_player = pygame.sprite.Group()
-group_player.add(player)
+pygame.display.set_caption("Fire And Water")
+clock = pygame.time.Clock()
+levels = []
+#mapa - 0 zem(grass) 1 platforma 2 dvere 3 coin 4 ohen 5 voda 6 jed 7 enemy 8 boy 9 girl
+# 41 ohen vlevo 49 ohen vpravo 51 voda vlevo 59 voda vpravo 61 jed vlevo 69 jed vpravo 
 
 
-run = True
-while run:
-	draw_bg()
-	world.draw()
-	group_player.update()
-	group_player.draw(screen)
-#draw_grid()
+running = True
+while running:
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			run = False
+		if event.type == QUIT:
+			running = False	
+
 	pygame.display.update()
+	clock.tick(FPS)
+
 pygame.quit()
+sys.exit()

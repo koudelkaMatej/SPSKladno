abbb18d4f8a59b9287575e554530987ab61e4d77
koudelkaMatej <koudelkamatje@seznam.cz>
2026-02-02 10:43:43 +0100

    fire and water

---
+++ PyGame/Fire And Water_IT3/main.py
@@ -0,0 +1,35 @@
+import pygame
+from settings import *
+from Level import Level
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Fire and Water Game")
+clock = pygame.time.Clock()
+
+dirt_image = pygame.image.load(DIRT_IMAGE_PATH).convert_alpha()
+dirt_image = pygame.transform.scale(dirt_image, (TILE_SIZE, TILE_SIZE))
+lava_image = pygame.image.load(LAVA_IMAGE_PATH).convert_alpha()
+lava_image = pygame.transform.scale(lava_image, (TILE_SIZE, TILE_SIZE))
+
+level = Level()
+def draw_level():
+    pocet_radku = len(level.level_data)
+    pocet_sloupcu = len(level.level_data[0])
+
+    for radek in range(pocet_radku):
+        for sloupec in range(pocet_sloupcu):
+            if level.level_data[radek][sloupec] == 1:  # dirt tile
+                screen.blit(dirt_image, (sloupec * TILE_SIZE, radek * TILE_SIZE))
+            if level.level_data[radek][sloupec] == 2:  # lava tile
+                screen.blit(lava_image, (sloupec * TILE_SIZE, radek * TILE_SIZE))
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(BLACK)
+    draw_level()
+    pygame.display.flip()
+    clock.tick(FPS)
---
+++ PyGame/Fire And Water_IT3/settings.py
@@ -0,0 +1,16 @@
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+FPS = 60
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+#level settings
+LEVEL_PATH = "Assets/Levels/level{}.csv"
+TILE_SIZE = 40
+DIRT_IMAGE_PATH = "Assets/Images/dirt.png"
+LAVA_IMAGE_PATH = "Assets/ohen a voda/lava/1.png"

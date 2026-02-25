c0448edc70717f8dd209a7b932ff4b399e739f69
koudelkaMatej <koudelkamatje@seznam.cz>
2026-02-25 11:34:46 +0100

    aa

--- Základy python 2026/testing.py
+++ Základy python 2026/testing.py
@@ -1,19 +1,10 @@
-# === Zpracování strukturovaných dat ===
-# Nejprve vytvoříme ukázkový soubor:
-with open("znamky.txt", "w") as f:
-    f.write("Adam 1 2 1 3\n")
-    f.write("Bára 2 1 1 2\n")
-    f.write("Cyril 3 2 4 2\n")
-    f.write("Dana 1 1 1 1\n")
-
-# Nyní ho přečteme a zpracujeme:
-print(f"{'Jméno':<10} {'Známky':<15} {'Průměr':>6}")
-print("-" * 35)
-
-with open("znamky.txt", "r") as f:
-    for radek in f:
-        casti = radek.strip().split()     # ["Adam", "1", "2", "1", "3"]
-        jmeno = casti[0]                   # "Adam"
-        znamky = [int(z) for z in casti[1:]]  # [1, 2, 1, 3]
-        prumer = sum(znamky) / len(znamky)
-        print(f"{jmeno:<10} {str(znamky):<15} {prumer:>6.2f}")
+#TODO
+vstup = int(input("Zadej číslo: "))
+print("Zadal jsi číslo: ",vstup)
+suma = 0
+for radek in range(1,vstup+2):
+    for sloupec in range(1,radek):
+        print(sloupec,end="")
+        suma += sloupec
+    print()
+print(suma)
